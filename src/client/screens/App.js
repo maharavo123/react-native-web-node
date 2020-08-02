9c5475d5b7b17eaa2ad5c116439b290fe2ffe290
devJSFullStack <ra.pierrodev@gmail.com>
2020-08-03 02:21:30 +0300

    Fix build

--- src/client/screens/App.js
+++ src/client/screens/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import AuthStack from './auth';
-import AppNavigation from './Main';
+import AppNavigation from './navigation';
 
 export class App extends React.Component {
   constructor(props) {
