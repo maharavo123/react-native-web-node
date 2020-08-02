23015b59b71f0ed1c7fb97fdc977158bd3af0c19
devJSFullStack <ra.pierrodev@gmail.com>
2020-08-02 16:04:17 +0300

    x

--- src/client/screens/Layout.js
+++ src/client/screens/Layout.js
@@ -1,9 +1,7 @@
 import React, { PureComponent } from 'react';
-import { SafeAreaView, ScrollView, Dimensions } from 'react-native';
+import { SafeAreaView, ScrollView } from 'react-native';
 
 import styles from 'css/screens/layout.css';
-
-const { width, height } = Dimensions.get('window');
 
 export default class AuthComponent extends PureComponent {
   constructor(props) {
