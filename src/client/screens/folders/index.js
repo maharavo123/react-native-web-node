b5994c857948397fff7c209054ba1660be355b9f
ra.pierrodev <ra.pierrodev@gmail.com>
2020-10-03 23:33:59 +0300

    Add data in search

--- src/client/screens/folders/index.js
+++ src/client/screens/folders/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Text, View, TextInput, Image, TouchableOpacity, FlatList } from 'react-native';
+import { Text, View, TextInput, Image, TouchableOpacity, Dimensions } from 'react-native';
 import { connect } from 'react-redux';
 import Select from 'react-select';
 
@@ -9,6 +9,11 @@ import mapDispatchToProps from 'mapDispatchToProps';
 import images from 'images';
 
 import styles from './styles.css';
+
+const dm = Dimensions.get('window').width;
+const width_d = (93 * dm / 100) - 80;
+
+const width_pourc = (pourc) => (pourc * width_d / 100);
 
 const optionsTypeAudit = [
   { value: 'Existant', label: 'Existant' },
@@ -110,8 +115,7 @@ class FoldersScreen extends Component {
           <View style={{
             justifyContent: 'space-between',
             flexDirection: 'row',
-            marginRight: 50,
-            marginLeft: 50,
+            width: width_d,
           }}>
             <View style={{
               justifyContent: 'space-between',
@@ -154,8 +158,74 @@ class FoldersScreen extends Component {
                 </View>
               </View>
             </View>
-            {PositioningActionsColumn()}
           </View>
+          <View
+            style={{
+              justifyContent: 'space-between',
+              flexDirection: 'row',
+              backgroundColor: '#2C7AC3',
+              width: width_d,
+              height: 45,
+              borderRadius: 5,
+              padding: 5,
+              marginBottom: 3
+            }}
+          >
+            <View style={{ width: width_pourc(50) / 5, justifyContent: 'center' }}>
+              <Text style={{ textAlign: 'center' }}>Référence dosier</Text>
+            </View>
+            <View style={{ width: width_pourc(50) / 2, justifyContent: 'center' }}>
+              <Text style={{ textAlign: 'center' }}>Nom du client</Text>
+            </View>
+            <View style={{ width: width_pourc(50) / 2, justifyContent: 'center' }}>
+              <Text style={{ textAlign: 'center' }}>Ville</Text>
+            </View>
+            <View style={{ width: width_pourc(50) / 5, justifyContent: 'center' }}>
+              <Text style={{ textAlign: 'center' }}>Code Postall</Text>
+            </View>
+            <View style={{ width: width_pourc(50) / 4, justifyContent: 'center' }}>
+              <Text style={{ textAlign: 'center' }}>Type Audit</Text>
+            </View>
+            <View style={{ width: width_pourc(50) / 4, justifyContent: 'center' }}>
+              <Text style={{ textAlign: 'center' }}>PDF Audit / Annex</Text>
+            </View>
+          </View>
+          {
+            folders && folders.map(({ _id, nom_client, vile_client, reference_document, code_client, type_audit }) => {
+              return (<View
+                key={_id}
+                style={{
+                  justifyContent: 'space-between',
+                  flexDirection: 'row',
+                  backgroundColor: '#FFFFFF',
+                  width: width_d,
+                  height: 45,
+                  borderRadius: 5,
+                  padding: 5,
+                  marginBottom: 3
+                }}
+              >
+                <View style={{ width: width_pourc(50) / 5, justifyContent: 'center' }}>
+                  <Text style={{ textAlign: 'center' }}>{reference_document}</Text>
+                </View>
+                <View style={{ width: width_pourc(50) / 2, justifyContent: 'center' }}>
+                  <Text style={{ textAlign: 'center' }}>{nom_client}</Text>
+                </View>
+                <View style={{ width: width_pourc(50) / 2, justifyContent: 'center' }}>
+                  <Text style={{ textAlign: 'center' }}>{vile_client}</Text>
+                </View>
+                <View style={{ width: width_pourc(50) / 5, justifyContent: 'center' }}>
+                  <Text style={{ textAlign: 'center' }}>{code_client}</Text>
+                </View>
+                <View style={{ width: width_pourc(50) / 4, justifyContent: 'center' }}>
+                  <Text style={{ textAlign: 'center' }}>{type_audit}</Text>
+                </View>
+                <View style={{ width: width_pourc(50) / 4, justifyContent: 'center' }}>
+                  <Text style={{ textAlign: 'center' }}>PDF Audit / Annex</Text>
+                </View>
+              </View>)
+            })
+          }
         </View>
       </View>
     );
